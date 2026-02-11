41fc9e4dd91c1c1fae07c74d1528476bc9f1f16d
Deepak-0707 <deepakoff2006@gmail.com>
2026-02-11 05:56:40 +0530

    modifed the dockerbuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,9 @@
 def call(String imageTag) {
     echo "Building Docker image ${imageTag}"
-    sh "docker build -t ${imageTag} ."
+
+    if (isUnix()) {
+        sh "docker build -t ${imageTag} ."
+    } else {
+        bat "docker build -t ${imageTag} ."
+    }
 }
